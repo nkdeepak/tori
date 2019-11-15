839de8e786ec71aa249623bbf3a57f029e91df7b
Krishna Nallamilli <deepak.nallamilli@gmail.com>
2019-11-15 13:53:13 +0530

    Added required classes

---
+++ mail.py
@@ -0,0 +1,18 @@
+"""
+    The main class used to send mail to the required user.
+"""
+
+
+class Mail():
+    
+    def __init__():
+        pass
+
+    def get_user_email():
+        pass
+
+    def send_mail():
+        pass
+
+    def format_mail():
+        pass
---
+++ pocket.py
@@ -0,0 +1,20 @@
+
+"""
+    The main pocket class which performs all the fucntions related to getting data from pocket and also formatting
+
+"""
+
+
+class Pocket():
+
+    def __init__():
+        pass
+
+    def perform_oauth():
+        pass
+
+    def get_list(tags):
+        pass
+
+    def format_item(item):
+        pass
